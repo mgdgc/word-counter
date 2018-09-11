767673acf9a2582dd11824c5ffb081eff1f2b3ca
Peter Choi <soc06212@gmail.com>
2018-09-11 10:58:31 +0900

    Initial Commit

---
+++ word count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  word count
+//
+//  Created by Peter Choi on 2018. 9. 11..
+//  Copyright © 2018년 RiDsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ word countTests/word_countTests.swift
@@ -0,0 +1,34 @@
+//
+//  word_countTests.swift
+//  word countTests
+//
+//  Created by Peter Choi on 2018. 9. 11..
+//  Copyright © 2018년 RiDsoft. All rights reserved.
+//
+
+import XCTest
+@testable import word_count
+
+class word_countTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
